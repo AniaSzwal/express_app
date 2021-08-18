1dd9822a95be9c683f2ef4119876284ad2c1aa14
Anna Szwal <aniaszwal@gmail.com>
2021-08-18 19:06:49 +0200

    picture 404 doesn't work

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 const app = express();
 
+function isUser() {
+    return false;
+};
+
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.use((req, res, next) => {
     res.show = (name) => {
         res.sendFile(path.join(__dirname, `/views/${name}`));
@@ -10,28 +16,25 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/user', (req, res, next) => {
+    if(isUser()) next();
+    else res.show('forbidden.html');
+});
+
 app.get('/', (req, res) => {
-    res.show('index.html');
+    res.show('home.html');
+});
+
+app.get('/home', (req, res) => {
+    res.show('home.html');
 });
 
 app.get('/about', (req, res) => {
     res.show('about.html');
 });
 
-app.get('/contact', (req, res) => {
-    res.show('contact.html');
-});
-
-app.get('/info', (req, res) => {
-    res.show('info.html');
-});
-
-app.get('/history', (req, res) => {
-    res.show('history.html');
-});
-
 app.use((req, res) => {
-    res.status(404).send('404 not found...');
+    res.status(404).show('404.html');
 })
 
 app.listen(8000, () => {
